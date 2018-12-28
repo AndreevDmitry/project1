71fe64138399932f4419dab9335434b22de9be92
Dmitry Andreev <andreev.dmitriy.1990@mail.ru>
2018-12-28 20:32:39 +0500

    Added 3 lines

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-// 'Hello World!' program 
- 
+// 'Hello World!' program
+
 #include <iostream>
- 
+#include <math>
+
 int main()
 {
   std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello World2!" << std::endl;
+  std::cout << "Hello World3!" << std::endl;
+  std::cout << "Hello World4!" << std::endl;
   return 0;
 }
