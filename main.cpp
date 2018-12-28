4d75e2edf3620e6931051dfa86e896bbe95ba745
AndreevDmitry <46165782+AndreevDmitry@users.noreply.github.com>
2018-12-28 20:37:25 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
   std::cout << "Hello World!" << std::endl;
   std::cout << "Hello World2!" << std::endl;
-  std::cout << "Hello World3!" << std::endl;
+  std::cout << "Hello World31!" << std::endl;
   std::cout << "Hello World4!" << std::endl;
   return 0;
 }
